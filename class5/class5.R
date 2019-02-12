c2f2d738cca515974eba3de510170b7a47b4d9d1
Nasha Luevit <nluevit@ucsd.edu>
2019-02-12 11:07:06 -0800

    to upload to github

--- class5/class5.R
+++ class5/class5.R
@@ -2,6 +2,7 @@
 #'title: "Class 05 R Graphics Intro"
 #'author: Nasha Luevit
 #'date: Jan 22nd, 2019
+#'output: github_document
 #' ---
 
 # Class 05 R Graphics Intro
@@ -22,6 +23,7 @@ boxplot(x, horizontal = TRUE)
 #read.table("weight_chart.txt", header = TRUE)
 #read.table("weight_chart.txt", header = FALSE)
 weight <- read.table("bimm143_05_rstats/weight_chart.txt", header = TRUE)
+
 #graph weight chart with changes
 #plot(weight)
 plot(weight, typ = "o", pch = 15, cex = 1.5, lwd = 2, ylim = c(2,10), xlab = "Age (months)", ylab = "Weight (kg)", main = "Baby Weight with Age")
@@ -29,17 +31,21 @@ plot(weight, typ = "o", pch = 15, cex = 1.5, lwd = 2, ylim = c(2,10), xlab = "Ag
 #import feature counts
 #read.table("feature_counts.txt", header = TRUE, sep = "\t")
 feature_counts <- read.table("bimm143_05_rstats/feature_counts.txt", header = TRUE, sep = "\t")
+
 #plot feature counts
 barplot(height = feature_counts[,2])
 barplot(feature_counts$Count)
 barplot(feature_counts$Count, horiz = TRUE, names.arg = feature_counts$Feature, main = "Number of Features in the Mouse GRCm38 Genome", las = 1, xlim = c(0,80000))
+
 #change margins so we can see the labels 
 par()$mar
 par(mar = c(3.1, 11.1, 4.1, 2))
 barplot(feature_counts$Count, horiz = TRUE, names.arg = feature_counts$Feature, main = "Number of Features in the Mouse GRCm38 Genome", las = 1, xlim = c(0,80000))
+
 #import male female counts
 #read.table("male_female_counts.txt", header = TRUE, sep = "\t")
 male_female_counts <- read.table("bimm143_05_rstats/male_female_counts.txt", header = TRUE, sep = "\t")
+
 #plot male female counts
 barplot(male_female_counts$Count, names.arg = male_female_counts$Sample, ylab = "Counts", col = rainbow(10))
 barplot(male_female_counts$Count, names.arg = male_female_counts$Sample, ylab = "Counts", col = rainbow(nrow(male_female_counts)))
